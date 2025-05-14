9e6c964cc3bfda36342c5f5b66d151cce2e36b85
JoaoVlopess <joaovlopes.carvalho@gmail.com>
2025-05-14 20:50:17 -0300

    add input login, add sing in button, add sing up Link and add Loter Logo

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,7 +1,23 @@
-export const LoginPage = () => {
-return (
-    <div className="LoginPage">
+import styles from "./LoginPage.module.css";
+import { Link } from 'react-router-dom';
 
-    </div>
-);
+
+export const LoginPage = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.login_area}>
+                <img src="/public/img/loter_Logo.png" alt="Logo Loter" />
+                <h1 className={styles.login_area_titulo}>Bem vindo de volta, dev!</h1>
+                <div className={styles.login_area_cadastro}>
+                    <p>Chegando pela primeira vez?</p>
+                    <a><Link to="/cadastro" >Cadastre-se!</Link></a>
+                </div>
+                <div className={styles.login_area_input}>
+                    <input type="email" placeholder="Seu email" />
+                    <input type="password" placeholder="********" />
+                </div>
+                <button className={styles.signin_button}>Entrar</button>
+            </div>
+        </div>
+    );
 }
