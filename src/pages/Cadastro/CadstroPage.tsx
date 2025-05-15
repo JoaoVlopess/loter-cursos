74f5a5762efa3f31331de3a5c98f384cb23f1132
JoaoVlopess <joaovlopes.carvalho@gmail.com>
2025-05-14 21:46:38 -0300

    Cadastropage creation and styling

--- src/pages/Cadastro/CadstroPage.tsx
+++ src/pages/Cadastro/CadstroPage.tsx
@@ -1,7 +1,23 @@
-export const CadastroPage = () => {
-return (
-    <>
-    <h1>ola2</h1>
-    </>
-);
+import styles from './CadastroPage.module.css';
+import { Link } from 'react-router-dom';
+
+export function CadastroPage() {
+  return (
+    <div className={styles.container}>
+      <img src="/public/img/loter_Logo.png" alt="Logo Loter" />
+
+      <h1>Crie sua conta</h1>
+      <p>
+        Já tem uma conta? <Link to="/">Entrar</Link>
+      </p>
+
+      <form className={styles.form}>
+        <input type="text" placeholder="Seu nome" />
+        <input type="email" placeholder="Seu email" />
+        <input type="password" placeholder="Senha" />
+        <input type="number" placeholder="Idade" />
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
 }
