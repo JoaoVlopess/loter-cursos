f8425600b2fd682fcc65f9cc8bc6e8ef3eabe444
JoaoVlopess <joaovlopes.carvalho@gmail.com>
2025-05-20 15:22:27 -0300

    create backend

---
+++ client/src/components/Curso/CursoGrid/CursoGrid.tsx
@@ -0,0 +1,60 @@
+import { CardCurso } from "../CardCurso/CardCurso";
+import type { Curso } from "../../../types/Curso/curso";
+import styles from "./CursoGrid.module.css";
+
+const cursos: Curso[] = [
+  {
+    id_curso: 1,
+    titulo: "Fundamentos de React",
+    descricao: "Aprenda os conceitos básicos do React, como componentes, props e estado.",
+    carga_horaria: 12,
+    id_professor: 1,
+    modulos: 5
+  },
+  {
+    id_curso: 2,
+    titulo: "Fundamentos de pyton",
+    descricao: "Aprenda os conceitos básicos do pyton, como IA.",
+    carga_horaria: 5,
+    id_professor: 3,
+    modulos: 10
+  },
+  {
+    id_curso: 3,
+    titulo: "Fundamentos de React",
+    descricao: "Aprenda os conceitos básicos do React, como componentes, props e estado.",
+    carga_horaria: 12,
+    id_professor: 1,
+    modulos: 5
+  },
+  {
+    id_curso: 4,
+    titulo: "Fundamentos de React",
+    descricao: "Aprenda os conceitos básicos do React, como componentes, props e estado.",
+    carga_horaria: 12,
+    id_professor: 1,
+    modulos: 5
+  }, {
+    id_curso: 5,
+    titulo: "Fundamentos de React",
+    descricao: "Aprenda os conceitos básicos do React, como componentes, props e estado.",
+    carga_horaria: 12,
+    id_professor: 1,
+    modulos: 5
+  },
+];
+
+export const CursoGrid = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.title}>Meus Cursos</h2>
+        <div className={styles.course_grid}>
+          {cursos.map((curso) => (
+            <CardCurso key={curso.id_curso} curso={curso} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import helmet from 'helmet'
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.listen(3000, () => {
+    console.log('Servidor rodando no link: http://localhost:3000');
+});
