edaef9e82b242b3dbdb70ec4816d85e383f20b80
JoaoVlopess <joaovlopes.carvalho@gmail.com>
2025-05-28 08:33:04 -0300

    feat: add title prop to CursoGrid in PlataformaPage; update tsconfig include path

--- client/src/pages/Plataforma/PlataformaPage.tsx
+++ client/src/pages/Plataforma/PlataformaPage.tsx
@@ -54,7 +54,7 @@ export const PlataformaPage = () => {
     <div className={styles.plataforma_page}>
       <NavBar />
       <div className={styles.main_content}>
-        <CursoGrid cursos={cursos} CardComponent={CardCurso} />
+        <CursoGrid cursos={cursos} CardComponent={CardCurso} titulo="Meus Cursos"/>
       </div>
       <Footer />
     </div>
